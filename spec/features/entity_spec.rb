6826b6b6725613f5b43babbc1bf3a9ca6a69ec0a
Anas <anas.chaabane98@gmail.com>
2022-12-22 16:28:00 +0100

    feature testing

--- spec/features/entity_spec.rb
+++ spec/features/entity_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
 
-RSpec.feature "Groups", type: :feature do
-
+RSpec.feature "Entities", type: :feature do
   before(:each) do
     @user = User.create(name: 'arbouch', email: 'arbouch@gmail.com', password: 'password')
     @group = Group.create(user: @user, name: 'Food', icon: 'https://icon_url')
+    @entity = Entity.create(user: @user, name:"pizzHut", amount:15)
+    @group.entities << @entity
     visit new_user_session_path
     fill_in 'Email', with: 'arbouch@gmail.com'
     fill_in 'Password', with: 'password'
     click_button 'Log in'
+    click_on(@group.name)
   end
 
   scenario 'I can see the group card.' do
     expect(page).to have_css('div.card')
   end
   scenario 'I can see the page title.' do
-    expect(page).to have_content 'Categories'
+    expect(page).to have_content 'Transactions'
   end
   scenario 'I can see the group name.' do
-    expect(page).to have_content 'Food'
+    expect(page).to have_content 'pizzHut'
   end
   scenario 'I can see the group total transaction.' do
-    expect(page).to have_content 'Credit'
+    expect(page).to have_content '15$'
   end
 end
